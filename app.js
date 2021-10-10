18c9b877769dc172e4defe70f7c0c4adac54d17f
lio85 <lionelprats2021@gmail.com>
2021-10-10 20:14:28 -0300

    subiendo proyecto a Heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req,res) => { res.sendFile(path.join(__dirname, 'views/index.html'
 app.use('/', categoryRoutes);
 
 
-app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
 
 
 
