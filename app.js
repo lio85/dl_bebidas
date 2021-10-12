f55ba53bb648c97a9fcbd3bd1844092a2afba680
lio85 <lionelprats2021@gmail.com>
2021-10-11 21:48:46 -0300

    11octubre

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express= require ('express');
 const app= express();
 const path= require('path'); 
-
 const categoryRoutes= require('./src/routes/productRoutes');
 
+app.use (express.static('./public'));
 
-app.get('/', (req,res) => { res.sendFile(path.join(__dirname, 'views/index.html'))});
+app.set ('view engine', 'ejs');
+
+// app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.get('/', (req,res) => { 
+    //res.sendFile(path.resolve(__dirname, 'views/index.html'))
+    res.render('index');
+});
+
 app.use('/', categoryRoutes);
-
 
 app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
 
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,16 @@
 const path= require('path');
 module.exports= {
     show_category: (req,res)=> {
-        res.sendFile(path.join(__dirname, '../../views/products/aperitivos.html'));
+        const categories= ['aperitivos','cervezas','combos','gaseosas','vinos','whiskeys'];
+        let view_path;
+        for (category of categories){
+            if(req.params.id== category){
+                res.render(`products/${category}`)
+                view_path= '../../views/products/'+category+'.html';
+                res.sendFile(path.join(__dirname, view_path));
+            }
+        }
+        
     }
 }
 
