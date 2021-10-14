e958415c774a52d20821fcd6ffd7f4167201aa45
lio85 <lionelprats2021@gmail.com>
2021-10-14 17:35:37 -0300

    vamos

--- src/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const productController= require('./../controllers/productController');
-
-router.get('/:id', productController.show_category);
-
-module.exports= router;
