75315ab1146d8c5f5902f496b2df442d5f0b034e
lio85 <lionelprats2021@gmail.com>
2021-10-14 17:29:15 -0300

    vamos

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set ('view engine', 'ejs');
 
 app.get('/', (req,res) => { 
     //res.sendFile(path.resolve(__dirname, 'views/index.html'))
-    res.render('index');
+    res.res.send('index');
 });
 
 app.use('/', categoryRoutes);
