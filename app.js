9febcd932c784a30760e724b7515f8180935e0e2
lio85 <lionelprats2021@gmail.com>
2021-10-14 17:55:35 -0300

    probando

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ var express= require('express');
 
 const app= express();
 
-app.get('/', (req,res)=> {`<h1>PITUCO</h1>`})
+app.get('/pituco', (req,res)=> {`<h1>PITUCO</h1>`})
+
+app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
