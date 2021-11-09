3569076f85c5bf1d779c30b64d11870dcc2d9325
lio85 <lionelprats2021@gmail.com>
2021-11-08 22:29:57 -0300

    probando api en mobile

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -11,7 +11,7 @@ for (let i=0; i< items__nav.length; i++){
     });
 };
 
-fetch("http://dl-bebidas-dashboard.herokuapp.com/api/productsbycategory")
+fetch("https://dl-bebidas-dashboard.herokuapp.com/api/productsbycategory")
     .then(function(response){
         return response.json();
     })
